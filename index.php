fd338f02ffdf9b38d7a915de5905367a276892b3
Alejandro Morales <alejandromorjim@gmail.com>
2022-03-23 10:58:30 -0600

    clase CreateUsser

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+Class CreateUsser{
+    public $name;
+    public $pass;
+    public function __construct($name, $pass)
+    {
+        $this->name = $name;
+        $this->pass = $pass; 
+    }
+}
